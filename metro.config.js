36b7af1fc19209cb868eaa892cef9f1a68fdfb44
fernandomenuk <menuk.fernando@revocaresolutions.com>
2025-11-05 12:22:17 +0530

    setup nativewind

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+let config = getDefaultConfig(__dirname);
 
 // Exclude expo-sqlite WASM files from web bundle
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'wasm');
+config = withNativeWind(config, { input: './src/global.css' });
 
 module.exports = config;
