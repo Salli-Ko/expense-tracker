4e041d68c1e6b85cf12cbf0b57271c6fdd000df1
fernandomenuk <menuk.fernando@revocaresolutions.com>
2025-11-05 13:28:33 +0530

    Revert "setup nativewind"
    This reverts commit 36b7af1fc19209cb868eaa892cef9f1a68fdfb44.

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
-let config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 // Exclude expo-sqlite WASM files from web bundle
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'wasm');
-config = withNativeWind(config, { input: './src/global.css' });
 
 module.exports = config;
