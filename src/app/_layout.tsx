a2b11f2b8b58199f63ac1ea6b2f4aed615a5d062
fernandomenuk <menuk.fernando@revocaresolutions.com>
2025-11-05 14:34:36 +0530

    feat(expense-tracker): add native wind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,8 @@
+import 'react-native-reanimated';
+import "../../global.css";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import ErrorBoundary from '@/components/ErrorBoundary';
